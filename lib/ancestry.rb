a911ad651af4d2e051aa768d68dc71c93ea5ebc2
Stephen Gooberman-Hill <stephen.gooberman-hill@amey.co.uk>
2016-01-13 14:35:04 +0000

    added ancestry pattern for UUID

--- lib/ancestry.rb
+++ lib/ancestry.rb
@@ -5,4 +5,5 @@ require_relative 'ancestry/has_ancestry'
 
 module Ancestry
   ANCESTRY_PATTERN = /\A[0-9]+(\/[0-9]+)*\Z/
+  ANCESTRY_UUID_PATTERN = ~/\A(?<uuid>\p{XDigit}{8}-\p{XDigit}{4}-\p{XDigit}{4}-\p{XDigit}{4}-\p{XDigit}{12})(\/\g<uuid>)*\Z/
 end
